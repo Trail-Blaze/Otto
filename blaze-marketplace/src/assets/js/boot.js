90ca455f1514a4afec1bc4083e965df730c1e65e
XMLTest200 <rexydogwoof01@gmail.com>
2021-11-04 13:24:12 -0600

    Settings sync, fortnite launching, other tweaks

--- blaze-marketplace/src/assets/js/boot.js
+++ blaze-marketplace/src/assets/js/boot.js
@@ -6,7 +6,9 @@ let __drivename =
   os.platform == "win32" ? process.cwd().split(path.sep)[0] : "/";
 let blazeDir = path.join(__drivename, "/Blaze/Launcher/");
 let helpersDir = path.join(blazeDir, "/helpers/");
+let backendDir = path.join(blazeDir, "/backend/")
 let userAssetsDir = path.join(blazeDir, "/userAssets/");
+const launcherConfig = require(path.join(blazeDir, "settings.json")); // Opening settings file for readOnly
 let installLength;
 let installList;
 let counter = 0;
@@ -16,6 +18,14 @@ const exec_options = {
   maxBuffer: 200 * 1024,
   killSignal: "SIGTERM",
   cwd: helpersDir,
+  env: null,
+};
+
+const exec_options2 = {
+  timeout: 0,
+  maxBuffer: 200 * 1024,
+  killSignal: "SIGTERM",
+  cwd: backendDir,
   env: null,
 };
 
@@ -142,17 +152,100 @@ function sendID(clicked_id) {
   console.log(clicked_id);
   let thisID = clicked_id;
   let thisElement = document.getElementById(thisID);
+  thisElement.classList.remove("bg-red-300"); 
+  thisElement.classList.remove("color-white");
+  thisElement.classList.add("bg-yellow-300");
+  thisElement.classList.add("color-black");
   thisElement.innerText = "Loading...";
 
-  exec(
-    `kickstart "${installList.InstallList[thisID].location}" -nobe -fromfl=eac -fltoken=87a0c99d9aa3ab5bb6a36C25`, exec_options,
-    (error, stdout, stderr) => {
+// Remove old script if any
+
+  exec("del runner.bat", exec_options,
+
+(error, stdout, stderr) => {
       if (error) {
         console.log(`error: ${error.message}`);
         return;
       }
       if (stderr) {
         console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+
+// Create new script
+
+  exec(`echo kickstart "${installList.InstallList[thisID].location}" -nobe -fromfl=eac -fltoken=87a0c99d9aa3ab5bb6a36C25 ${launcherConfig.bypassMethod} ${installList.InstallList[thisID].logonAs}> runner.bat`, exec_options,
+
+(error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        thisElement.innerText = "Failed. 😢";
+        thisElement.classList.remove("bg-yellow-300");
+        thisElement.classList.remove("color-black");
+        thisElement.classList.add("bg-red-300"); 
+        thisElement.classList.add("color-white");
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        thisElement.innerText = "Failed. 😢";
+        thisElement.classList.remove("bg-yellow-300");
+        thisElement.classList.remove("color-black");
+        thisElement.classList.add("bg-red-300");
+        thisElement.classList.add("color-white");
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+
+// Start the backend...
+
+exec(`StartMainServiceModule.lnk`, exec_options2,
+
+(error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        thisElement.innerText = "Backend Failed. 🥶";
+        thisElement.classList.remove("bg-yellow-300");
+        thisElement.classList.remove("color-black");
+        thisElement.classList.add("bg-red-300"); 
+        thisElement.classList.add("color-white");
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        thisElement.innerText = "Backend Failed. 🥶";
+        thisElement.classList.remove("bg-yellow-300");
+        thisElement.classList.remove("color-black");
+        thisElement.classList.add("bg-red-300");
+        thisElement.classList.add("color-white");
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+
+// Run the thang...
+
+  exec("legacyBoot.lnk", exec_options,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        thisElement.innerText = "Failed. 😢";
+        thisElement.classList.remove("bg-yellow-300");
+        thisElement.classList.remove("color-black");
+        thisElement.classList.add("bg-red-300"); 
+        thisElement.classList.add("color-white");
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        thisElement.innerText = "Failed. 😢";
+        thisElement.classList.remove("bg-yellow-300");
+        thisElement.classList.remove("color-black");
+        thisElement.classList.add("bg-red-300");
+        thisElement.classList.add("color-white");
         return;
       }
       console.log(`stdout: ${stdout}`);
