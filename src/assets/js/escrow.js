0bc40a0faf3a9fdcccac708aaee91a84f05f565c
AlexDev404 <alexdev404@users.noreply.github.com>
2022-04-12 17:35:05 -0600

    Update

--- src/assets/js/escrow.js
+++ src/assets/js/escrow.js
@@ -8,8 +8,8 @@ let userAssetsDir = path.join(blazeDir, "/userAssets/");
 let status = document.getElementById("reviewStatus");
 
 if (!fs.existsSync(path.join(userAssetsDir, "\\InstallList.json"))) {
-  status.innerHTML = "WELCOME TO BLAZE! HAVE FUN!<br/><p>LIVE-BUILD_1<br/><code>[2022-04-12T08:29:06.726Z]</code></p>";
-  setTimeout(window.location.replace("firststartup.html"), 5000);
+  status.innerText = "WELCOME TO BLAZE! HAVE FUN!";
+  window.location.replace("firststartup.html");
 } else {
   status.innerText = "ROUTING..."
   window.location.replace("boot.html");
