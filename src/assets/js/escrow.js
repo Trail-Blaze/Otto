e6e439364577d59e823f0833772941298c1aa030
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-04-12 02:29:51 -0600

    Update escrow.js

--- src/assets/js/escrow.js
+++ src/assets/js/escrow.js
@@ -8,7 +8,7 @@ let userAssetsDir = path.join(blazeDir, "/userAssets/");
 let status = document.getElementById("reviewStatus");
 
 if (!fs.existsSync(path.join(userAssetsDir, "\\InstallList.json"))) {
-  status.innerText = "WELCOME TO BLAZE! HAVE FUN!";
+  status.innerHTML = "WELCOME TO BLAZE! HAVE FUN!<br/><p>LIVE-BUILD_1 <code>[2022-04-12T08:29:06.726Z]</code></p>";
   window.location.replace("firststartup.html");
 } else {
   status.innerText = "ROUTING..."
