7d228e659e3927e9892d581011cb52e5b264ad0c
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-05-16 17:41:59 -0600

    Cleans up code a bit. Edit functionality still doesn't work as yet.

--- src/assets/js/edit.js
+++ src/assets/js/edit.js
@@ -1,23 +1,26 @@
 // Set delay on script as we know people still use Blaze on potato PCs * cough * * cough *
 setTimeout(() => {
-  const edit = document.getElementById("edit");
+  // Commit #134 - Add support to more entries
+  // const edit = document.getElementById("edit");
   const dropdown = document.getElementById("dropdown");
   const dropdown_c = document.getElementById("dropdown_cont");
 
-  edit.addEventListener("click", () => {
-    if (dropdown.classList.contains("hidden")) {
-      setTimeout(() => {
-        dropdown.classList.remove("hidden");
-      }, 100);
-      dropdown_c.style.height = "200px";
-      window.scroll({
-        top: document.documentElement.scrollTop + 180,
-        left: 0,
-        behavior: "smooth",
-      });
-      return;
-    }
-    dropdown.classList.add("hidden");
-    dropdown_c.style.height = "1px";
-  });
-}, 5000);
+  document.querySelectorAll("#edit").forEach((e) => {
+    e.addEventListener("click", () => {
+      if (dropdown.classList.contains("hidden")) {
+        setTimeout(() => {
+          dropdown.classList.remove("hidden");
+        }, 100);
+        dropdown_c.style.height = "200px";
+        window.scroll({
+          top: document.documentElement.scrollTop + 180,
+          left: 0,
+          behavior: "smooth",
+        });
+        return;
+      }
+      dropdown.classList.add("hidden");
+      dropdown_c.style.height = "1px";
+    });
+  }, 5000);
+});
