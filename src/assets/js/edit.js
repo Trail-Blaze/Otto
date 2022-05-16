8b0805b0611456c9dc67197ea7836a035d58e061
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-05-16 16:57:32 -0600

    THE BUGS

--- src/assets/js/edit.js
+++ src/assets/js/edit.js
@@ -3,13 +3,16 @@ setTimeout(() => {
   const edit = document.getElementById("edit");
   const dropdown = document.getElementById("dropdown");
   const dropdown_c = document.getElementById("dropdown_cont");
+
   edit.addEventListener("click", () => {
     if (dropdown.classList.contains("hidden")) {
-      dropdown.classList.remove("hidden");
+      setTimeout(() => {
+        dropdown.classList.remove("hidden");
+      }, 100);
       dropdown_c.style.height = "200px";
-    } else {
-      dropdown.classList.add("hidden");
-      dropdown_c.style.height = "1px";
+      return;
     }
+    dropdown.classList.add("hidden");
+    dropdown_c.style.height = "1px";
   });
 }, 5000);
