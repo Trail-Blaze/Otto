c53ba717217a64135248833b368954124ccf3331
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-05-16 17:44:26 -0600

    Comment file

--- src/assets/js/edit.js
+++ src/assets/js/edit.js
@@ -7,19 +7,28 @@ setTimeout(() => {
 
   document.querySelectorAll("#edit").forEach((e) => {
     e.addEventListener("click", () => {
+      // If hidden...
       if (dropdown.classList.contains("hidden")) {
+        // Wait 100ms...
         setTimeout(() => {
+          // Make dropdown visible
           dropdown.classList.remove("hidden");
         }, 100);
+        // Stretch UI pane to fit dropdown...
         dropdown_c.style.height = "200px";
+        // Scroll till visible
         window.scroll({
+          // Current-Y scroll position + 180px
           top: document.documentElement.scrollTop + 180,
           left: 0,
           behavior: "smooth",
         });
+        // Exit
         return;
       }
+      // Otherwise hide dropdown
       dropdown.classList.add("hidden");
+      // ...and reset UI pane height to default
       dropdown_c.style.height = "1px";
     });
   }, 5000);
