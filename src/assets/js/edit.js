35c789f87e47739968a5d4e145d80a6fee5c64a0
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-05-17 13:07:24 -0600

    Prevent container chopping

--- src/assets/js/edit.js
+++ src/assets/js/edit.js
@@ -5,33 +5,36 @@ setTimeout(() => {
   const dropdown = document.querySelectorAll('[id^="dropdown_"]');
   const dropdown_c = document.querySelectorAll('[id^="dcont_"]');
 
-  edit.forEach((e, i)=>{
-
-
+  edit.forEach((e, i) => {
     document.getElementById(e.id).addEventListener("click", () => {
-    // If hidden...
-    if (dropdown[i].classList.contains("hidden")) {
-      // Wait 100ms...
-      setTimeout(() => {
-        // Make dropdown visible
-        dropdown[i].classList.remove("hidden");
-      }, 100);
-      // Stretch UI pane to fit dropdown...
-      dropdown_c[i].style.height = "200px";
-      // Scroll till visible
-      window.scroll({
-        // Current-Y scroll position + 180px
-        top: document.documentElement.scrollTop + 180,
-        left: 0,
-        behavior: "smooth",
-      });
-      // Exit
-      return;
-    }
-    // Otherwise hide dropdown
-    dropdown[i].classList.add("hidden");
-    // ...and reset UI pane height to default
-    dropdown_c[i].style.height = "1px";
+      // If hidden...
+      if (dropdown[i].classList.contains("hidden")) {
+        // Wait 100ms...
+        setTimeout(() => {
+          // Make dropdown visible
+          dropdown[i].classList.remove("hidden");
+        }, 100);
+        // Stretch UI pane to fit dropdown...
+        dropdown_c[i].style.height = "200px";
+        // Stretch Main Container to fit entry...
+        main.style.height =
+          (
+            main.style.height.slice(0, main.style.height.length - 2) + 45
+          ).toString() + "vh";
+        // Scroll till visible
+        window.scroll({
+          // Current-Y scroll position + 180px
+          top: document.documentElement.scrollTop + 180,
+          left: 0,
+          behavior: "smooth",
+        });
+        // Exit
+        return;
+      }
+      // Otherwise hide dropdown
+      dropdown[i].classList.add("hidden");
+      // ...and reset UI pane height to default
+      dropdown_c[i].style.height = "1px";
+    });
   });
-});
 }, 5000);
