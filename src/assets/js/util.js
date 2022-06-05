d28a1d7c2e3ebbf59b042681f5b97b94773567c6
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-06-05 15:21:27 -0600

    Experimental Server Listings

---
+++ src/assets/js/util.js
@@ -0,0 +1,25 @@
+function changeID(oldid, newID) {
+  e = document.getElementById(oldid.toString());
+  e.id = newID;
+}
+
+function removeID(oldid) {
+  e = document.getElementById(oldid.toString());
+  e.id = "";
+}
+
+function sendID(clicked_id) {
+  console.log(clicked_id);
+  ipc.send("sendState", clicked_id);
+  ipc.send("reqPageSwitch", "package_viewer.html");
+}
+ref = (id) => document.getElementById(id);
+
+function sleep(ms = parseInt(ms)) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
