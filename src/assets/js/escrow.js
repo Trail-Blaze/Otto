52b252760245e6ee2404b15a4c61e21ccd823430
AlexDev404 <alexdev404@users.noreply.github.com>
2022-08-27 13:31:51 -0600

    Cherrypick from Volta

--- src/assets/js/escrow.js
+++ src/assets/js/escrow.js
@@ -3,7 +3,7 @@ let status_ = document.getElementById("reviewStatus");
 // amIbanned();
 
 if (!fs.existsSync(path.join(userAssetsDir, "\\InstallList.json"))) {
-  status_.innerHTML = "WELCOME TO VOLTAIC! HAVE FUN!<br/><p>DEV-BUILD_51<br/><code>[2022-08-04T22:47:01.762Z]</code></p>";
+  status_.innerHTML = "WELCOME TO BLAZE! HAVE FUN!<br/><p>DEV-BUILD_51<br/><code>[2022-08-04T22:47:01.762Z]</code></p>";
   setTimeout(()=>{window.location.replace("firststartup.html")}, 4500);
 } else {
   status_.innerText = "ROUTING..."
